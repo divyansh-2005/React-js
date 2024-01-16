e3052c218bc534d2e025b05eaa8d45e839c7eea0
Divyansh Kumbhat <divyanshkumbhat@gmail.com>
2024-01-16 18:41:16 +0530

    custom react

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -3,12 +3,12 @@ import Chai from "./chai"
 
 
 function App() {
- 
+  const username = "DIVYA"
 
   return (
     <>
     <Chai />
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
     <p>test para</p>
     </>
   )
