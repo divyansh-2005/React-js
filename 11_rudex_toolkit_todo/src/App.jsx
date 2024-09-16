4da6ca1b47220ead1be0d66c7abca28eee1acd0f
divyansh-2005 <divyanshkumbhat@gmail.com>
2024-09-17 00:09:16 +0530

    Redux Toolkit

---
+++ 11_rudex_toolkit_todo/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todos'
+import { Provider } from 'react-redux'
+import { store } from './app/store'
+
+function App() {
+  
+  return (
+    <Provider store={store}>
+
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo/>
+      <Todos/>
+      
+    </Provider>
+  )
+}
+
+export default App
